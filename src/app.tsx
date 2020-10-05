b7e6b7f58b25fb48102ea45b93a4d0a5d5c860bb
amankkg <amankubanychbek@gmail.com>
2020-10-05 18:31:35 +0600

    feat(client): add PersonForm w/o submit logic

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,8 @@
 import React, {useEffect, useState} from 'react'
 
+import {PersonForm} from './person-form'
 import logo from './logo.svg'
 import './app.css'
-
-const ping = () =>
-  fetch(import.meta.env.SNOWPACK_PUBLIC_API_URL)
-    .then((resp) => resp.text())
-    .then((data) => alert(data))
 
 export const App = () => {
   const [count, setCount] = useState(0)
@@ -25,8 +21,9 @@ export const App = () => {
           Page has been open for <code>{count}</code> seconds
         </p>
       </header>
-      <br />
-      <button onClick={ping}>Ping?</button>
+      <main className="app-body">
+        <PersonForm />
+      </main>
     </div>
   )
 }
