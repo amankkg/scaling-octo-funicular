56e1c1cf31c609e58e092e8cfd9c7164c35426fa
amankkg <amankubanychbek@gmail.com>
2020-10-05 16:19:44 +0600

    fix(client): update deps

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,9 +3,12 @@ module.exports = {
     public: '/',
     src: '/_dist_',
   },
+  devOptions: {
+    open: 'none',
+  },
   plugins: [
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
     ['@snowpack/plugin-run-script', {cmd: 'tsc --noEmit', watch: '$1 --watch'}],
   ],
-};
+}
